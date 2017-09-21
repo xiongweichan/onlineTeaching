704a4b28f68155c652894161df52491b40468ccc
xiongweichan <xiongweichan@gmail.com>
2017-09-21 13:25:11 +0800

    同上

--- client/TeacherClient/TeacherClient/SettingWIndow.xaml.cs
+++ client/TeacherClient/TeacherClient/SettingWIndow.xaml.cs
@@ -69,6 +69,7 @@ namespace TeacherClient
         private void Btn_ClearCacheClick(object sender, RoutedEventArgs e)
         {
             CacheHelper.ClearCache();
+            Model.CacheCount = CacheHelper.GetCacheCount().ToString("f1") + "M";
         }
     }
     public class SettingModel : ViewModelBase
